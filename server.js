f74cfc3a088311ba8f7b4f450ea3dfd0f7a4b259
kydtrai11 <138143346+kydtrai11@users.noreply.github.com>
2025-09-14 23:28:38 +0700

    asd

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ app.use(cors({
 }));
 
 /* ===== Middleware ===== */
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ limit: '100mb', extended: true }));
 app.use(cookieParser());
 
 /* ===== Static Files ===== */
