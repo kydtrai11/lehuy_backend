14a62ab3621bedbcf8f0690785c16ffe35ae0d04
kydtrai11 <138143346+kydtrai11@users.noreply.github.com>
2025-09-27 17:16:29 +0700

    ád

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const router = express.Router();
 
 // Thư mục uploads nằm trong project
-const descDir = path.join(process.cwd(), 'uploads');
+const descDir = path.join(process.cwd(), '../uploads');
 if (!fs.existsSync(descDir)) {
   fs.mkdirSync(descDir, { recursive: true });
 }
@@ -23,8 +23,8 @@ const upload = multer({ storage });
 
 // Upload 1 ảnh
 router.post('/', upload.single('image'), (req, res) => {
+  console.log(req.file);
   if (!req.file) return res.status(400).json({ error: 'No file uploaded' });
-
   // Chỉ trả về URL public, không phải đường dẫn tuyệt đối trong container
   const url = `/uploads/${req.file.filename}`;
   res.json({ url });
